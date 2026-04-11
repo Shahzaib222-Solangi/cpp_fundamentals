e98ee8bc49a7db7ae96e22d6d7fb65fc54b282e7
Shahzaib222-Solangi <shahzaibalisolangi222@gmail.com>
2026-04-11 15:19:18 +0500

    AddedTaking_Inputs_from_User.cpp for user input example

---
+++ 01-Basics/Taking_Inputs_from_User.cpp
@@ -0,0 +1,21 @@
+//Question:1
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter first integer : ";
+    cin>>a;
+    int b;
+    cout<<"Enter second integer : ";
+    cin>>b;
+    float c;
+    cout<<"Enter first float : ";
+    cin>>c;
+    float d;
+    cout<<"Enter second float : ";
+    cin>>d;
+    char ch;
+    cout<<"Enter a character : ";
+    cin>>ch;
+    return 0;
+}
