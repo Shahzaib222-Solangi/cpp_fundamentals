997aa9874d3eb4ac3bacec84d38abd520296fd7f
Shahzaib222-Solangi <shahzaibalisolangi222@gmail.com>
2026-04-11 15:16:48 +0500

    Added Diamond Pattern printing program in C++

---
+++ 01-Basics/Diamond_Pattern.cpp
@@ -0,0 +1,28 @@
+//Question 5:
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "\t\t         *\n";
+    cout << "\t\t        ***\n";
+    cout << "\t\t       *****\n";
+    cout << "\t\t      *******\n";
+    cout << "\t\t     *********\n";
+    cout << "\t\t    ***********\n";
+    cout << "\t\t   *************\n";
+    cout << "\t\t  ***************\n";
+    cout << "\t\t *****************\n";
+    cout << "\t\t*******************\n";
+    cout << "\t\t *******************\n";
+    cout << "\t\t*********************\n";
+    cout << "\t\t *******************\n";
+    cout << "\t\t  *****************\n";
+    cout << "\t\t   ***************\n";
+    cout << "\t\t    *************\n";
+    cout << "\t\t     ***********\n";
+    cout << "\t\t      *********\n";
+    cout << "\t\t       *******\n";
+    cout << "\t\t        *****\n";
+    cout << "\t\t         ***\n";
+    cout << "\t\t          *\n";
+    return 0;
+}
