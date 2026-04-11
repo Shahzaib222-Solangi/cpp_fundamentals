23a666cc19a943ec0e8546e97a0a57fd1cba2c08
Shahzaib222-Solangi <shahzaibalisolangi222@gmail.com>
2026-04-11 15:22:53 +0500

    Added Student Marksheet program in C++

---
+++ 01-Basics/Student_Marksheet.cpp
@@ -0,0 +1,15 @@
+//Making Student Marksheet
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+cout<<"============================================================\n";
+cout<<setw(30)<<"MARKSHEET\n";
+cout<<"============================================================\n";
+cout<<"Shahzaib ICT score out of 100\t :\t  70.5\n";
+cout<<"Shahzaib MATH score out of 100\t :\t  80.7\n";
+cout<<"Shahzaib ENGLISH score out of 100 :\t  90.8\n";
+cout<<"obtain marks out of 300\t\t :\t  242";
+return 0;
+}
