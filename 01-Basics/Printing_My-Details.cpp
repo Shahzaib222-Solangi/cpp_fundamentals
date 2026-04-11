7a7525f4cfbc1ae24c8c0f88caa40d201db952db
Shahzaib222-Solangi <shahzaibalisolangi222@gmail.com>
2026-04-11 15:13:02 +0500

    Added Printing_My-Details.cpp to display user info

---
+++ 01-Basics/Printing_My-Details.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Name : Shahzaib\n";
+cout<<"CMS ID : 023-24-0131\n";
+cout<<"Department : Computer Science\n";
+cout<<"Batch : 2k24";
+return 0;
+}
