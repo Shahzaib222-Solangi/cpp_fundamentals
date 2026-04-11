eaaf0f5e25e37948031d7ac34cdfd182ee1ccb02
Shahzaib222-Solangi <shahzaibalisolangi222@gmail.com>
2026-04-11 15:25:27 +0500

    Added program to convert character to ASCII value
    This program takes a character input from the user and outputs its ASCII value.

---
+++ 01-Basics/Conversion_of_character_to_ASCII.cpp
@@ -0,0 +1,13 @@
+//Conversion of Character to its Ascci
+
+#include<iostream>
+using namespace std;
+int main(){
+char a;
+cout<<"Enter a character : ";
+cin>>a;
+cout<<"The ASCII value of A is : ";
+int x = (int)a;
+cout<<x;
+return 0;
+}
