8171ca229242348ea6d66e3586d1488f8d3ce298
Shahzaib222-Solangi <shahzaibalisolangi222@gmail.com>
2026-04-12 11:34:38 +0500

    Added program to check if two numbers are equal

---
+++ 01-Basics/Checking_TwoNumbers_Equal_or_Not.cpp
@@ -0,0 +1,13 @@
+//Checking Two Numbers are equal or not
+
+#include<iostream>
+using namespace std;
+int main(){
+int x,y;
+cout<<"Enter value of x : ";
+cin>>x;
+cout<<"Enter value of y : ";
+cin>>y;
+cout<<((x==y) || (x!=y));
+return 0;
+}
